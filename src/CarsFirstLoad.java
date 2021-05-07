8b4a7ff69148b4e0f0aa7a7d647e7dda0ae2410d
17802309 <kanna_andreevna@mail.ru>
2021-05-07 22:25:43 +0300

    07.05.2021

--- src/CarsFirstLoad.java
+++ src/CarsFirstLoad.java
@@ -6,13 +6,15 @@ import java.util.Map;
 public class CarsFirstLoad {
     public static Collection<Car> init (Collection<Car> cars){
         cars.add(new Car(1L,"BMW","Sedan"));
-        cars.add(new Car(2L,"BMW","Crossover"));
-        cars.add(new Car(3L,"BMW","Coupe"));
+        cars.add(new Car(2L,"Toyota","Crossover"));
+        cars.add(new Car(3L,"Kia","Coupe"));
         return cars;
     }
 
     public static Map <String,Car> init (Map<String, Car> cars){
         cars.put("1", new Car(1L,"BMW","Sedan"));
+        cars.put("2", new Car(2L,"Toyota","Crossover"));
+        cars.put("3", new Car(3L,"Kia","Coupe"));
         return cars;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,53 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        //Map<String, Car>  cars = new HashMap<>();
         List<Car> cars = new ArrayList<>();
         List<Car> sedanCars = new ArrayList<>();
-//        List<String> typesCar = new ArrayList<>();
+        List<Car> crossCars = new ArrayList<>();
 
         CarsFirstLoad.init(cars);
         CarsFirstLoad.print(cars);
 
-        HashSet<String> typesCar = new HashSet<String>();
-//        Сохраняем уникальные типы в HashSet
         for(Car car : cars){
-            typesCar.add(car.getType());
+            switch (car.getType()) {
+                case "Sedan":
+                    sedanCars.add(car);
+                    CarsFirstLoad.print(sedanCars);
+                    break;
+                case "Crossover":
+                    crossCars.add(car);
+                    CarsFirstLoad.print(crossCars);
+                    break;
+                default:
+                    break;
+            }
         }
-        System.out.println(typesCar);
-//
-        for(Car car : cars){
-//            for (String type : typesCar){
-//                if()
-//            }
-           sedanCars.add(car);
-           CarsFirstLoad.print(sedanCars);
+        System.out.println("-------------------------------------------------");
+        String str = "dog cat lion dog mouse dog tiger bird cat pig";
+        if (str.length() == 0) {
+            System.err.println("Пустая строка");
+            return;
         }
+//        HashSet гарантирует уникальность - то, что нужно
+        Set<String> set = new HashSet<String>();
+        String words[] = str.split("[^a-zA-Z]+");
+        int size = words.length, i;
+//        System.out.print("Все найденные слова в файле: ");
+        for (i = 0; i < size; ++i) {
+            set.add(words[i]);
+//            System.out.print(words[i] + " ");
+        }
+//        1
+        System.out.println("Количество различных слов в файле = " + set.size());
+
+//        2
+//        Объединение компараторов в цепочку
+//        Iterator iterator = set.iterator();
+//        while (iterator.hasNext()){
+//            Comparator<String> comparator = Comparator.comparing(set1 -> iterator.next());
+//            comparator = comparator.thenComparing(obj -> obj.iter().length());
+//        }
+
+
     }
 }
