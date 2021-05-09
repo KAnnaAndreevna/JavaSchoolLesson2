8ca6748626e0ff45652b885e4424b773277b7d97
17802309 <kanna_andreevna@mail.ru>
2021-05-09 21:07:28 +0300

    09.05.2021

---
+++ src/LenghtComparator.java
@@ -0,0 +1,6 @@
+//import java.util.Comparator;
+//
+//static class LenghtComparator implements Comparator<>{
+//    @Override
+//    public int compare ()
+//}
--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
 //        HashSet гарантирует уникальность - то, что нужно
         Set<String> set = new HashSet<String>();
         String words[] = str.split("[^a-zA-Z]+");
-        int size = words.length, i;
+        int size = words.length, i, j,count;
 //        System.out.print("Все найденные слова в файле: ");
         for (i = 0; i < size; ++i) {
             set.add(words[i]);
@@ -41,15 +41,39 @@ public class Main {
         }
 //        1
         System.out.println("Количество различных слов в файле = " + set.size());
-
+        System.out.println("-------------------------------------------------");
 //        2
-//        Объединение компараторов в цепочку
-//        Iterator iterator = set.iterator();
-//        while (iterator.hasNext()){
-//            Comparator<String> comparator = Comparator.comparing(set1 -> iterator.next());
-//            comparator = comparator.thenComparing(obj -> obj.iter().length());
-//        }
+        List<String> animalList = new ArrayList<>();
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            animalList.add(iterator.next().toString());
+        }
+        animalList.sort(new lengthComparator());
 
+//        3
 
+        String words2[][] = new String[size][2];
+        count = 1;
+        Arrays.sort(words);
+        for (i = 1, j = 0; i < size ; ++i, j++) {
+
+            if (words[i].equals(words[i - 1])) {
+                do {
+                    count++;
+                    i++;
+                } while(i < words.length && words[i].equals(words[i - 1]));
+            }
+            words2[i][0] = words[i-1];
+            words2[i][1] = Integer.toString(count);
+            System.out.println(words2[i][0] + "-" + words2[i][1]);
+            count = 1;
+        }
     }
 }
+
+class lengthComparator implements Comparator<String>{
+    @Override
+    public int compare (String st1, String st2) {
+        return st1.length() - st2.length();}
+}
+
